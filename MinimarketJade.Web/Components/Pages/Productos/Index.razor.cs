3f9e42857e0297dfa211157c75fc4c85f84f81a0
Joan <joanyavi17@gmail.com>
2026-03-03 21:02:26 -0400

    Sensor de inventario actualizado

--- MinimarketJade.Web/Components/Pages/Productos/Index.razor.cs
+++ MinimarketJade.Web/Components/Pages/Productos/Index.razor.cs
@@ -5,6 +5,8 @@ using MinimarketJade.Web.Services.Categorias;
 using MinimarketJade.Web.Services.Productos;
 using MinimarketJade.Web.Helpers;
 using Microsoft.AspNetCore.Components;
+using MinimarketJade.Web.Services;
+using MinimarketJade.Web.Services.Auth;
 
 namespace MinimarketJade.Web.Components.Pages.Productos;
 
@@ -12,6 +14,8 @@ public partial class Index : ComponentBase
 {
     [Inject] private IProductoService ProductoService { get; set; } = default!;
     [Inject] private ICategoriaService CategoriaService { get; set; } = default!;
+    [Inject] private IMovInventarioService MovInventarioService { get; set; } = default!;
+    [Inject] private AuthService AuthService { get; set; } = default!;
 
     private List<Producto> productos = new();
     private IReadOnlyList<CategoriaDto> categorias = Array.Empty<CategoriaDto>();
@@ -144,6 +148,20 @@ public partial class Index : ComponentBase
         {
             error = "❌ Error al conectar con la base de datos: " + ex.Message;
         }
+
+        //Mandar registros a MovInventario
+        if (producto.StockActual > 0)
+        {
+            await MovInventarioService.RegistrarAsync(
+                idProducto: producto.IdProducto,
+                tipoMovimiento: "Nuevo",
+                cantidad: producto.StockActual,
+                idUsuario: AuthService.CurrentUser!.IdUsuario,
+                motivo: $"Registro inicial de producto: {producto.Nombre}"
+            );
+        }
+
+
     }
 
     private async Task InhabilitarProducto(Producto p)
